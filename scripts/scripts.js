c200ac37e2d4db95c9fb3af829ccde719b98d955
Mike Duran <mike.duran@hotmail.de>
2023-04-08 15:32:59 +0200

    added the javascript that based on the link the content is adapted

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,3 +7,18 @@ function toggleLinks() {
   }
 }
 
+function afficherPage(page) {
+  var image = document.getElementById("image-center");
+  var paragraphe = document.getElementById("paragraphe-center");
+  if (page === "page1") {
+    image.src = "/images/image1.jpg";
+    paragraphe.innerHTML = "Contenu de la page 1";
+  } else if (page === "page2") {
+    image.src = "/images/image2.jpg";
+    paragraphe.innerHTML = "Contenu de la page 2";
+  } else {
+    // page d'accueil par défaut
+    image.src = "/images/test.jpg";
+    paragraphe.innerHTML = "Contenu de la page d'accueil";
+  }
+}
